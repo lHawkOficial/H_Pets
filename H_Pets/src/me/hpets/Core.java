3c2b8ed2d25a034cabc5fa01ff7ceb5ec6d87b28
lHawkOficial <luanvictorchagas2015@gmail.com>
2022-09-19 15:40:49 -0300

    updated

--- H_Pets/src/me/hpets/Core.java
+++ H_Pets/src/me/hpets/Core.java
@@ -1,7 +1,40 @@
 package me.hpets;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import lombok.Getter;
+
+@Getter
 public class Core extends JavaPlugin {
 
+	@Getter
+	private static Core instance;
+	private String tag, version = "§dv" + getDescription().getVersion();
+	
+	@Override
+	public void onEnable() {
+		instance = this;
+		saveDefaultConfig();
+		reloadPlugin();
+		
+		sendConsole(" ");
+		sendConsole(tag + " &aH_Pets iniciado com sucesso! &6[Author lHawk_] " + version);
+		sendConsole(" ");
+	}
+	
+	@Override
+	public void onDisable() {
+		sendConsole(" ");
+		sendConsole(tag + " &cH_Pets desligado com sucesso! &6[Author lHawk_] " + version);
+		sendConsole(" ");
+	}
+	
+	public void reloadPlugin() {
+		reloadConfig();
+		tag = getConfig().getString("Config.tag").replace("&", "§");
+	}
+	
+	public void sendConsole(String msg) {Bukkit.getConsoleSender().sendMessage(msg.replace("&", "§"));}
+	
 }
